35a782052110534bd9ca92fc869e937f74ea2efb
Khetreiqcer <jvpossenti666@gmail.com>
2025-07-19 09:54:29 -0300

    fix: ajustado variavel do postgres.

--- internal/config/config.go
+++ internal/config/config.go
@@ -13,7 +13,7 @@ type Config struct {
 
 func Load() *Config {
 	return &Config{
-		Port:                 getEnv("PORT", "5432"),
+		Port:                 getEnv("PORT", "3000"),
 		DatabaseURL:          getEnv("DATABASE_URL", "postgres://user:password@db:5432/rinha-backend?sslmode=disable"),
 		ProcessorDefaultURL:  getEnv("PROCESSOR_DEFAULT_URL", "http://payment-processor-default:8080"),
 		ProcessorFallbackURL: getEnv("PROCESSOR_FALLBACK_URL", "http://payment-processor-fallback:8080"),
--- internal/config/init.sql
+++
@@ -1,9 +0,0 @@
--- Conecte-se ao PostgreSQL como superuser ou usuário com privilégios
-CREATE DATABASE rinha_backend
-    WITH
-    OWNER = user
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.utf8'
-    LC_CTYPE = 'en_US.utf8'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
